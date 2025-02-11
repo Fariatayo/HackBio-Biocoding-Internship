2572821646110defa3f33ad2d12bd08b31b608a1
Fariatayo <116832892+Fariatayo@users.noreply.github.com>
2025-02-11 19:47:12 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+# Team Leucine
+team_name = "Leucine"
+
+name = ["Fariat Adigun", "Amogh Oke", "Olanrewaju Ishola"]
+slack_username = ["Fariat", "apo33717", "LarryShol"]
+email = ["adigunfariat@gmail.com", "okeap37@gmail.com", "olanrewajuishola93@gmail.com"]
+hobby = ["Walking", "Music", "Watching and Playing football"]
+country = ["United Kingdom", "India", "United Kingdom"]
+discipline = ["Microbiology", "Biology", "Microbiology"]
+preferred_programming_language = ["Python", "Python", "Python"]
+
+# Printing the organized information
+print("Team Name =", team_name, "\n")
+
+print("Name =", name[0])
+print("Slack Username =", slack_username[0])
+print("Email =", email[0])
+print("Hobby =", hobby[0])
+print("Country =", country[0])
+print("Discipline =", discipline[0])
+print("Preferred Programming Language =", preferred_programming_language[0])
+print("-")
+
+print("Name =", name[1])
+print("Slack Username =", slack_username[1])
+print("Email =", email[1])
+print("Hobby =", hobby[1])
+print("Country =", country[1])
+print("Discipline =", discipline[1])
+print("Preferred Programming Language =", preferred_programming_language[1])
+print("-")
+
+print("Name =", name[2])
+print("Slack Username =", slack_username[2])
+print("Email =", email[2])
+print("Hobby =", hobby[2])
+print("Country =", country[2])
+print("Discipline =", discipline[2])
+print("Preferred Programming Language =", preferred_programming_language[2])
+print("-")
+
+# Printing team data in line format
+print("\nTeam Data in Line Format:")
+print(name[0], "|", slack_username[0], "|", email[0], "|", hobby[0], "|", country[0], "|", discipline[0], "|", preferred_programming_language[0])
+print(name[1], "|", slack_username[1], "|", email[1], "|", hobby[1], "|", country[1], "|", discipline[1], "|", preferred_programming_language[1])
+print(name[2], "|", slack_username[2], "|", email[2], "|", hobby[2], "|", country[2], "|", discipline[2], "|", preferred_programming_language[2])
